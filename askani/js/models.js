d78586906f3a3d7359238a3c209a2619a9f21a6d
Alvaro Mouriño <alvaro@mourino.net>
2010-11-03 11:38:31 -0200

    Added all jslint options.

--- askani/js/models.js
+++ askani/js/models.js
@@ -1,8 +1,13 @@
 /*jslint
+         bitwise: true,
+         eqeqeq: true,
+         immed: true,
          indent: 4,
+         newcap: true,
          nomen: false,
          onevar: true,
          plusplus: true,
+         regexp: true,
          undef: true,
          white: true
 */
