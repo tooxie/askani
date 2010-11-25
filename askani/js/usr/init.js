b71da289f5b0054cbd3b298b3a2197febab00fa1
Alvaro Mouriño <alvaro@mourino.net>
2010-11-25 19:41:01 -0200

    Bugfix: Share capabilities removed on localhost.

--- askani/js/usr/init.js
+++ askani/js/usr/init.js
@@ -20,9 +20,6 @@ $(document).ready(function () {
     var gacode,
         host = window.location.hostname;
     if (host === 'askani.net' || host === 'www.askani.net') {
-        // Share
-        $('#shareaskani').remove();
-
         // Analytics
         $.get("js/usr/gacode.js", function (data) {
             gacode = data;
@@ -30,13 +27,15 @@ $(document).ready(function () {
                 $.gaTrack(gacode);
             }
         });
-    } else {
         stLight.options({
             publisher: '6cbefb55-0497-4fec-b520-0d552fb726d6',
             st_title: 'Askani - Django models generator',
             st_url: 'http://askani.net/',
             onhover: false
         });
+    } else {
+        // Share
+        $('#shareaskani').remove();
     }
     $('a[href^="http"]').live('click', function () {
         window.open(this.href);
